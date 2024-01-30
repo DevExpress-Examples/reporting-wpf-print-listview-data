70cf83668b5907dfbfa95229676284cc47c94c5f
DevExpressExampleBot <fakeEmail>
2024-01-30 23:54:04 +0400

    Source auto update [skip ci]

--- VB/SimpleDataSet.vb
+++ VB/SimpleDataSet.vb
@@ -1,75 +1,71 @@
-﻿Imports System
+Imports System
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Windows
 Imports System.Windows.Data
 
 Namespace WPFPrintListView
+
     Public Class SimpleDataSet
         Inherits DataSet
 
         Public Sub New()
             MyBase.New()
-            Dim table As New DataTable("table")
-
+            Dim table As DataTable = New DataTable("table")
             DataSetName = "ManualDataSet"
-
-            table.Columns.Add("ID", GetType(Int32))
+            table.Columns.Add("ID", GetType(Integer))
             table.Columns.Add("MyDateTime", GetType(Date))
             table.Columns.Add("MyRow", GetType(String))
             table.Columns.Add("MyData", GetType(Double))
             table.Constraints.Add("IDPK", table.Columns("ID"), True)
-
-            Tables.AddRange(New DataTable() { table })
+            Tables.AddRange(New DataTable() {table})
         End Sub
 
         Public Shared Function CreateData() As SimpleDataSet
-            Dim ds As New SimpleDataSet()
+            Dim ds As SimpleDataSet = New SimpleDataSet()
             Dim table As DataTable = ds.Tables("table")
-
-            Dim rnd As New Random(Date.Now.Millisecond)
-
-            For i As Integer = 0 To 99
-                table.Rows.Add(New Object() { i, Date.Today.AddDays(1), (If(i Mod 2 = 0, "A", "B")), Math.Round(rnd.NextDouble() * 100, 2) })
-            Next i
+            Dim rnd As Random = New Random(Date.Now.Millisecond)
+            For i As Integer = 0 To 100 - 1
+                table.Rows.Add(New Object() {i, Date.Today.AddDays(1), If(i Mod 2 = 0, "A", "B"), Math.Round(rnd.NextDouble() * 100, 2)})
+            Next
 
             Return ds
         End Function
 
-        #Region "Disable Serialization for Tables and Relations"
-        <DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)> _
-        Public Shadows ReadOnly Property Tables() As DataTableCollection
+'#Region "Disable Serialization for Tables and Relations"
+        <DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)>
+        Public Overloads ReadOnly Property Tables As DataTableCollection
             Get
                 Return MyBase.Tables
             End Get
         End Property
 
-        <DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)> _
-        Public Shadows ReadOnly Property Relations() As DataRelationCollection
+        <DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)>
+        Public Overloads ReadOnly Property Relations As DataRelationCollection
             Get
                 Return MyBase.Relations
             End Get
         End Property
-        #End Region ' Disable Serialization for Tables and Relations
+'#End Region  ' Disable Serialization for Tables and Relations
     End Class
 
-    <ValueConversion(GetType(Date), GetType(String))> _
+    <ValueConversion(GetType(Date), GetType(String))>
     Public Class DateConverter
         Implements IValueConverter
 
-        Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
-            Dim [date] As Date = DirectCast(value, Date)
+        Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+            Dim [date] As Date = CDate(value)
             Return [date].ToShortDateString()
         End Function
 
-        Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+        Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
             Dim strValue As String = TryCast(value, String)
-            Dim resultDateTime As Date = Nothing
+            Dim resultDateTime As Date
             If Date.TryParse(strValue, resultDateTime) Then
                 Return resultDateTime
             End If
+
             Return DependencyProperty.UnsetValue
         End Function
     End Class
-
 End Namespace
--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,34 +1,34 @@
-﻿Imports System
+Imports System
 Imports System.Windows
 Imports DevExpress.Xpf.Printing
 
 Namespace WPFPrintListView
-    Partial Public Class Window1
+
+    Public Partial Class Window1
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
         Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            listView1.DataContext = SimpleDataSet.CreateData().Tables(0)
+            Me.listView1.DataContext = SimpleDataSet.CreateData().Tables(0)
         End Sub
 
         Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            Dim link As New SimpleLink("ListViewDocument")
-            Dim preview As New DocumentPreviewWindow()
+            Dim link As SimpleLink = New SimpleLink("ListViewDocument")
+            Dim preview As DocumentPreviewWindow = New DocumentPreviewWindow()
             preview.PreviewControl.DocumentSource = link
-
-            link.PageHeaderTemplate = DirectCast(Resources("printHeaderTemplate"), DataTemplate)
-            link.DetailTemplate = DirectCast(Resources("printDataTemplate"), DataTemplate)
-            link.DetailCount = listView1.Items.Count
-            AddHandler link.CreateDetail, AddressOf link_CreateDetail
+            link.PageHeaderTemplate = CType(Resources("printHeaderTemplate"), DataTemplate)
+            link.DetailTemplate = CType(Resources("printDataTemplate"), DataTemplate)
+            link.DetailCount = Me.listView1.Items.Count
+            AddHandler link.CreateDetail, New EventHandler(Of CreateAreaEventArgs)(AddressOf link_CreateDetail)
             link.CreateDocument(True)
             preview.ShowDialog()
         End Sub
 
         Private Sub link_CreateDetail(ByVal sender As Object, ByVal e As CreateAreaEventArgs)
-            e.Data = listView1.Items(e.DetailIndex)
+            e.Data = Me.listView1.Items(e.DetailIndex)
         End Sub
     End Class
 End Namespace
